1e8516e2a99291e3457ffb9ad1909003b7b72393
Shreeram <shreeram.shanmugasundaram_entc21@pccoer.in>
2023-04-10 19:09:15 +0530

    Url change

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://localhost:5000/" });
+const API = axios.create({ baseURL: "" });
 
 API.interceptors.request.use((req) => {
   if (localStorage.getItem("profile")) {
     console.log("inside api insterceptors");
-    console.log(localStorage.getItem("profile").token)
-    req.headers.authorization = JSON.parse(localStorage.getItem("profile")).token;
-
+    console.log(localStorage.getItem("profile").token);
+    req.headers.authorization = JSON.parse(
+      localStorage.getItem("profile")
+    ).token;
   }
   return req;
 });
@@ -15,11 +16,10 @@ API.interceptors.request.use((req) => {
 export const signin = (formData) => API.post("/user/login", formData);
 export const signup = (formData) => API.post("/user/create", formData);
 
-
 export const getcolleges = (formData) =>
   API.post("/user/collegelist", formData);
-  
-export const getchance = (formData)=>{
-  API.post("user/predict",formData) 
-}
-export const authenticate = (formData)=> API.post("/authenticate",formData)
+
+export const getchance = (formData) => {
+  API.post("user/predict", formData);
+};
+export const authenticate = (formData) => API.post("/authenticate", formData);
